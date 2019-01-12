79731254b0cdb48ace5b2dc785e82926e4362973
Barry Kane <bazzakane@gmail.com>
2019-01-12 16:44:40 +0000

    Comment HumidityTester.py
    Comments added in order to remember what it does in thirty years.

--- HumidityTester.py
+++ HumidityTester.py
@@ -1,11 +1,11 @@
+#Get everything imported.
 from sense_hat import SenseHat
 from time import sleep
 from datetime import datetime
 import time
-
+#Standard definitions
 sense = SenseHat()
-
-
+#Start the loop 48 times, as the process waits 5 minutes, times 48 equals four hours.
 for i in range(48):
     humidity = sense.get_humidity()
     temp = sense.get_temperature()
@@ -14,7 +14,6 @@ for i in range(48):
     print("Humidity: %s %%rH" % humidity)
     print("Temperature: %s C" % temp)
     sleep(300)
+#Show message when done!
 else:
     sense.show_message("Done!")
-    
-
